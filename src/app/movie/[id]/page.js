21c16d5ad6d5b139f1b9cfd1f1468a7e4b7797f4
Roshantiwary1 <tiwariroshan9999@gmail.com>
2023-02-08 20:25:25 +0530

    fixed deployment issue

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,10 +1,20 @@
-import Image from "next/image";
 
-async function page({params}) {
+import Image from "next/image";
+// import { useState } from "react";
+import { ReactPlayer } from 'react-player/youtube';
+
+async function Page({params}) {
+
 
     const movieId =params.id;
-    const res =  await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`)
+    const res =  await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=5dc2347f57ee0146e493090dcf36ef21`)
     const movie = await res.json()
+
+    const res1 = await fetch(`https://api.themoviedb.org/3/movie/615777/videos?api_key=5dc2347f57ee0146e493090dcf36ef21`);
+
+    const trailer = await res.json()
+
+    console.log(trailer.results[0])
     // console.log(params.id)
     // console.log(movie)
   return (
@@ -16,11 +26,24 @@ async function page({params}) {
         <h1 className="text-lg mb-3 font-bold">{movie.name || movie.title}</h1>
         <p className="text-lg mb-3"><span className="font-bold mr-1">Overview : </span>{movie.overview}</p>
         <p className="text-lg mb-3"><span className="font-bold mr-1">Release Date : </span>{movie.release_date || movie.first_air_date}</p>
-        <p className="text-lg mb-3"><span className="font-bold mr-1">Rating : </span>{ movie.vote_average.toFixed(1)}</p>
+        <p className="text-lg mb-3"><span className="font-bold mr-1">Rating : </span>{ movie.vote_average}</p>
+        {/* <button className="mt-5 bg-red-500 px-4 py-2 text-white rounded-lg" onClick={()=>{setShow(true);setVideoid(movieId)}}>Watch Trailer</button> */}
      </div>
+     <div className="">
+            <div className="opacityLayer"></div>
+            <div className="videoPlayer">
+                <ReactPlayer
+                    url={`https://www.youtube.com/watch?v=${trailer.results[0].key}`}
+                    controls
+                    width="100%"
+                    height="100%"
+                    // playing={true}
+                />
+            </div>
+        </div>
      </div>
     </div>
   )
 }
 
-export default page
+export default Page
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ const API_KEY = process.env.API_KEY
 export default async function Home({searchParams}) {
 
   const genre = searchParams.genre || "fetchTrending"
-
+  console.log(searchParams.id)
 
   const res = await fetch(`https://api.themoviedb.org/3/${genre==="fetchTopRated"?"movie/top_rated":"trending/all/week"}?api_key=${API_KEY}`,{next:{revalidate:100000}});
   const data =await res.json();
